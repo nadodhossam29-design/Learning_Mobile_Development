2171c29be80cd254407e9bfea93755cbb5fe4f67
Nada Hossam <nadodhossam29@gmail.com>
2026-03-10 01:36:47 +0200

    Update task1.kt

--- task1.kt
+++ task1.kt
@@ -1,4 +1,13 @@
-class task1 {
 
 
+fun main() {
+    println("please enter score")
+    val score = readln().toInt()
+
+    when (score) {
+        in 90..100 -> println("A , Excellent")
+        in 80..89 -> println("B , Good")
+        in 70..79 -> println("C , Average")
+        in 0..69 -> println("F , Needs Work")
+    }
 }
